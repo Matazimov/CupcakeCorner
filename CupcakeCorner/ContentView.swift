7367c2d75c5dc395bee76b852293211b8258ab8a
matazimov <ilkhomzhonmatazimov@gmail.com>
2022-04-13 18:26:48 +0500

    Работа с асинхронным получением данных

--- CupcakeCorner/ContentView.swift
+++ CupcakeCorner/ContentView.swift
@@ -7,10 +7,47 @@
 
 import SwiftUI
 
+struct Response: Codable {
+    let results: [MyResult]
+}
+
+struct MyResult: Codable {
+    let trackId: Int
+    let trackName: String
+    let collectionName: String
+}
+
 struct ContentView: View {
+    @State private var results = [MyResult]()
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        List(results, id: \.trackId) { item in
+            VStack(alignment: .leading) {
+                Text(item.trackName)
+                    .font(.headline)
+                Text(item.collectionName)
+            }
+        }
+        .task {
+            await loadData()
+        }
+    }
+    
+    func loadData() async {
+        guard let url = URL(string: "https://itunes.apple.com/search?term=taylor+swift&entity=song") else {
+            print("Invalid URL")
+            return
+        }
+        
+        do {
+            let data = try await URLSession.shared.data(from: url)
+            
+            if let decodedResponse = try? JSONDecoder().decode(Response.self, from: data.0) {
+                self.results = decodedResponse.results
+            }
+        } catch {
+            print("Invalid data")
+        }
     }
 }
 
