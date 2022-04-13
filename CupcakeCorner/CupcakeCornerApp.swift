520c71293a21fc7667b781d9d4dbb514aa4d23bb
matazimov <ilkhomzhonmatazimov@gmail.com>
2022-04-13 16:51:40 +0500

    Initial Commit

---
+++ CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Ilkhomzhon on 13.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
